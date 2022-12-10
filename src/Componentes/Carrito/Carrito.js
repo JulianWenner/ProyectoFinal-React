880845b0df065ae8fe9c233643f82017dff83ada
wenner <julianwenner@gmail.com>
2022-12-10 20:44:22 -0300

    proyecto-final

---
+++ src/Componentes/Carrito/Carrito.js
@@ -0,0 +1,12 @@
+import "./style.css";
+import logoCarrito from "../../Imagenes/logoCarrito.jpg"
+
+const Carrito = () => {
+  return (
+    <div className="imagnes-logo">
+      <img className="carrito" src={logoCarrito} alt="img-logo"/>
+    </div>
+  )
+}
+
+export default Carrito
---
+++ src/Componentes/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import "./style.css";
+import Carrito from "../Carrito/Carrito";
+
+const NavBar = () => {
+  return (
+    <div className="ClaseContenedora">
+        <div className="NavBar">
+            <h1>Mi tienda</h1>   
+            <ul className="list">
+                <li className="items" ><a  className="enlace" href="#">PRODUCTOS</a></li>
+                <li className="items"><a className="enlace" href="#">OFERTAS</a></li>
+                <li className="items"><a className="enlace" href="#">CONTACTO</a></li>
+                <li className="items"><a className="enlace" href="#"></a></li>
+
+            </ul>
+            <Carrito/>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
